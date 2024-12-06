4c9bc2af8cdd7399e347e451a1d876799d6da0c4
solssak <gswoong1012@gmail.com>
2024-12-06 15:00:43 +0900

    feat: Check environment variable type

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        // 환경변수에 대한 타입 체크
+        BACKEND_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
