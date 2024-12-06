301f89ad758d384e66bc531dec3d2feb88289e96
solssak <gswoong1012@gmail.com>
2024-12-06 16:11:11 +0900

    feat: Create a table using typeorm

--- src/proudct/entities/proudct.entity.ts
+++ src/proudct/entities/proudct.entity.ts
@@ -1,1 +1,17 @@
-export class Proudct {}
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from '../../common/base.entity';
+
+@Entity()
+export class Product extends BaseEntity {
+  @Column()
+  name: string;
+
+  @Column()
+  desc: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  productImg: string;
+}
