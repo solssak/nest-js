a2e07bf9ee870d521b2f637cfb90ff0c8755dbb4
solssak <gswoong1012@gmail.com>
2024-12-06 18:00:12 +0900

    feat: Implement validation pipes

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('api');
+
+  app.enableCors();
+  // app.enableCors({
+  //   origin: configService.get<string>('FRONTEND_URL'),
+  // });
+
+  // validation pipes
+  app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('EliceLab_Test')
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,15 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ example: 'iPhone 16 Pro' })
   name: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ example: 'so good' })
   desc: string;
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty({ example: 1000000 })
   price: number;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ example: 'http://example.com/1' })
   productImg: string;
 }
