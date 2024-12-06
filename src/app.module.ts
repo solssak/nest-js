ffc4ca6d0b40df23bdcd1ad37569c0e63144f446
solssak <gswoong1012@gmail.com>
2024-12-06 15:39:03 +0900

    feat: Initailized typeORMmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import * as Joi from '@hapi/joi';
 
 @Module({
@@ -17,6 +18,7 @@ import * as Joi from '@hapi/joi';
         POSTGRES_DB: Joi.string().required(),
       }),
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
